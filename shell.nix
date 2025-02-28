88d2d5c823352e0fd67f313c9e7ab49f9b71bf97
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2025-02-27 17:12:44 -0800

    Nix Shell for Python development

---
+++ shell.nix
@@ -0,0 +1,27 @@
+# shell.nix
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2025-02-25. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3a05eebede89661660945da1f151959900903b6a.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python313.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      pandas
+      numpy
+      nptyping
+      requests
+      jupyter
+      jupyter-core
+      pyngo
+      jupyterlab
+      ipykernel
+      matplotlib
+      django
+      django-types
+      ipywidgets
+      ipython
+      ipympl
+    ]))
+  ];
+}
